61cbd050e518cb96de7b55411f88b1fe32b7bf85
Agathe Puissant <52568496+AgathePuissant@users.noreply.github.com>
2023-04-26 10:32:32 +0200

    Update Dimorphism.R

--- Analysis/code/Dimorphism.R
+++ Analysis/code/Dimorphism.R
@@ -50,30 +50,37 @@ dF<-as.matrix(proxy::dist(meanphen_F)) #female/female difference
 
 
 dratio <- dM/dF #ratio between every pair of species
+dratio_FM = -(dF/dM-1)
+dratio_MF = (dM/dF-1)
 
 library(diverge)
 sis=extract_sisters(subtree) #get the sister speceis
 sis$ratio <- 0
 sis$dm <- 0
 sis$df <- 0
+sis$ratio_FM <- 0
+sis$ratio_MF <- 0
 
 #Get the ratio for each pair of sister species
 for (sp in 1:dim(sis)[1]){ 
   sis[sp,]$ratio<-dratio[sis[sp,1],sis[sp,2]]
+  sis[sp,]$ratio_FM<-as.numeric(dratio_FM[sis[sp,1],sis[sp,2]])
+  sis[sp,]$ratio_MF<-as.numeric(dratio_MF[sis[sp,1],sis[sp,2]])
   sis[sp,]$dm <-dM[sis[sp,1],sis[sp,2]]
   sis[sp,]$df <-dF[sis[sp,1],sis[sp,2]]
 }
 
 #Find max dimorphism value for the pair
 sis$dimorphism <- apply(cbind(meanphen_dimorph[sis$sp1,],meanphen_dimorph[sis$sp2,]),1,max)
+sis$newratio = apply(sis,1,function(x) if(as.numeric(x[4])<0){return (as.numeric(x[4]))} else{return(as.numeric(x[5]))})
 
 
-val= sis$ratio
+val= sis$newratio
 names(val) <- sis$sp1
 
 p<-ggtree(subtree, layout="circular")#plot_genres(subtree)
 p2 <- gheatmap(p,data.frame(val),offset=1, width=.1, colnames = F, legend_title = "Ratio contrast male / contrast female")+
-  scale_fill_gradient2(midpoint = 1, na.value="white", mid="lightyellow")+
+  scale_fill_gradient2(midpoint = 0, na.value="white", mid="lightyellow")+
   labs(fill="Ratio contrast male / contrast female")
 
 library(ggnewscale)
@@ -96,9 +103,9 @@ sis$bin<-sis$dimorphism>0.3
 sis$bin <- factor(sis$bin)
 levels(sis$bin) <- c("Monomorphic\nLess than 0.3","Dimorphic\nAbove 0.3")
 
-densdimorph <- ggplot(sis, aes(x=ratio,y=factor(bin),fill=after_stat(x), height = stat(density)))+
+densdimorph <- ggplot(sis, aes(x=newratio,y=factor(bin),fill=after_stat(x), height = stat(density)))+
   geom_density_ridges_gradient(jittered_points=T, point_alpha = .3,quantile_lines = TRUE, quantiles = 2)+
-  scale_fill_gradient2(midpoint=1, mid="lightyellow") +
+  scale_fill_gradient2(midpoint=0, mid="lightyellow") +
   theme_bw()+
   xlab("Ratio contrast male / contrast female")+
   ylab("Density")+ 
@@ -110,6 +117,6 @@ densdimorph
 
 ############## Test whether the ratio is less than one for dimorphic species in average
 
-wilcox.test(sis[sis$dimorphism>0.3,]$ratio, mu=1, alternative = "less")
+wilcox.test(sis[sis$dimorphism>0.3,]$newratio, mu=0, alternative = "less")
 
 ##############
